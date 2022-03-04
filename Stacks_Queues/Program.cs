aff333d49c06bccc5679efc90d539eefcb3867f1
Rushi <189489rishi@gmail.com>
2022-03-04 21:35:46 +0530

    Ability to peak pop from stack

--- Stacks_Queues/Program.cs
+++ Stacks_Queues/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿   // Ability to peak pop from stack
+using System;
 
 namespace Stacks_Queues
 {
@@ -10,6 +11,7 @@ namespace Stacks_Queues
             linkListStack.Push(70);
             linkListStack.Push(30);
             linkListStack.Push(56);
+            linkListStack.Peak_Pop();
             linkListStack.Display();
         }
     }
